64b6d5f19d132deb5ad90faabb0d78afceeb9bc2
virendra <singh.virendra@orangemantra.in>
2018-01-20 18:25:25 +0530

    commit changes

--- App2.java
+++ App2.java
@@ -3,6 +3,7 @@ package com.jenkigs;
 public class App2 {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome");
 		for(int i=1;i<100;i++){
 			System.out.println("hello :"+i);
 		}
